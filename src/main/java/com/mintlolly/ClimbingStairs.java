8cb9021db4d13f5ef42075d994dd303a60fc7b0a
jiangbo <mintlolly@outlook.com>
2021-07-13 17:34:18 +0800

    爬楼梯

---
+++ src/main/java/com/mintlolly/ClimbingStairs.java
@@ -0,0 +1,26 @@
+package com.mintlolly;
+
+/**
+ * Created on 2021/7/13
+ *
+ * @author jiangbo
+ * Description:
+ * 假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+ * 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+ * 注意：给定 n 是一个正整数。
+ *  1 -> 1种
+ *  1+1,2 ->2 种
+ *  1+1+1,1+2,2+1 -> 3种
+ *  1+1+1+1,1+1+2,1+2+1,2+1+1,2+2, -> 5种
+ */
+public class ClimbingStairs {
+    public Integer climbingStairs(int n){
+
+        return null;
+    }
+
+    public static void main(String[] args) {
+        ClimbingStairs climbingStairs = new ClimbingStairs();
+        System.out.println(climbingStairs.climbingStairs(4));
+    }
+}
